b644f9bc5a746719d16bb28bff5363c2bba94e24
kselladu@asu.edu <kselladu@asu.edu>
2016-09-30 15:12:16 -0700

    Server Error resolved

--- ccloud/admin.py
+++ ccloud/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Containers, RequestQueue
 
 # Register your models here.
+admin.site.register(Containers)
+admin.site.register(RequestQueue)
