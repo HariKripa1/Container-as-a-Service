2b801d528eda074ced1946067397da9afd3423b5
Your Name <ssaibab1@asu.edu>
2016-10-26 01:22:22 -0700

    Models updated for cluster

--- ccloud/models.py
+++ ccloud/models.py
@@ -6,6 +6,84 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Cluster(models.Model):
+    STATUS_FORCREATE = 'STATUS_FORCREATE'
+    STATUS_FORMODIFY = 'STATUS_FORMODIFY'
+    STATUS_FORDELETE = 'STATUS_FORDELETE'
+    STATUS_CREATED = 'STATUS_CREATED'
+    STATUS_DELETED = 'STATUS_DELETED'
+    STATUS_MODIFIED = 'STATUS_MODIFIED'
+    STATUS_CREATE_FAILED = 'STATUS_CREATE_FAILED'
+    STATUS_MODIFY_FAILED = 'STATUS_MODIFY_FAILED'
+    STATUS_DELETE_FAILED = 'STATUS_DELETE_FAILED'
+    STATUS_CHOICES = (
+        (STATUS_FORCREATE,'Creation in progress'),
+        (STATUS_FORMODIFY,'Modification in progress'),
+        (STATUS_FORDELETE,'Deletion in progress'),
+        (STATUS_CREATED,'Created'),
+        (STATUS_DELETED,'Delete'),
+        (STATUS_MODIFIED,'Modified'),
+        (STATUS_CREATE_FAILED,'Clutser creation failed'),
+        (STATUS_MODIFY_FAILED,'Cluster redeployment failed'),
+        (STATUS_DELETE_FAILED,'Cluster deletion failed')
+    )
+    cluster_name = models.CharField(max_length=200)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    status=models.CharField(max_length=100,choices=STATUS_CHOICES)
+    no_of_instances = models.IntegerField()
+    requested_no_of_instance = models.IntegerField()
+    master_ip = models.CharField(max_length=200)
+    token_id = models.CharField(max_length=500)
+    creation_date = models.DateTimeField('date created')
+    last_update_date = models.DateTimeField('date updated')
+    created_by=models.CharField(max_length=100)
+    def __str__(self):
+        return self.cluster_name
+
+class Node(models.Model):
+    MASTER_Y = 'Y'
+    MASTER_N = 'N'
+    MASTER_CHOICES = (
+        (MASTER_Y,'Yes'),
+        (MASTER_N,'No')
+    )
+    STATUS_FORCREATE = 'STATUS_FORCREATE'
+    STATUS_FORMODIFY = 'STATUS_FORMODIFY'
+    STATUS_FORDELETE = 'STATUS_FORDELETE'
+    STATUS_CREATED = 'STATUS_CREATED'
+    STATUS_DELETED = 'STATUS_DELETED'
+    STATUS_MODIFIED = 'STATUS_MODIFIED'
+    STATUS_CREATE_FAILED = 'STATUS_CREATE_FAILED'
+    STATUS_MODIFY_FAILED = 'STATUS_MODIFY_FAILED'
+    STATUS_DELETE_FAILED = 'STATUS_DELETE_FAILED'
+    STATUS_CHOICES = (
+        (STATUS_FORCREATE,'Creation in progress'),
+        (STATUS_FORMODIFY,'Modification in progress'),
+        (STATUS_FORDELETE,'Deletion in progress'),
+        (STATUS_CREATED,'Created'),
+        (STATUS_DELETED,'Delete'),
+        (STATUS_MODIFIED,'Modified'),
+        (STATUS_CREATE_FAILED,'Node creation failed'),
+        (STATUS_MODIFY_FAILED,'Node redeployment failed'),
+        (STATUS_DELETE_FAILED,'Node deletion failed')
+    )
+    cluster_id = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+    machine_ip = models.CharField(max_length=200)
+    master = models.CharField(max_length=1,choices=MASTER_CHOICES)
+    machine_name = models.CharField(max_length=200)
+    status=models.CharField(max_length=100,choices=STATUS_CHOICES)
+    
+class openstack_user(models.Model):
+    ADMIN = 'A'
+    USER = 'U'
+    LOOKUP_CHOICES = (
+        (ADMIN,'Admin'),
+        (USER,'User')
+    )
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)    
+    projectname = models.CharField(max_length=200)
+    role = models.CharField(max_length=1,choices=LOOKUP_CHOICES)
+    
 class Container(models.Model):
     APPLICATION_NODE_JS = 'APPLICATION_NODE_JS'
     APPLICATION_CHOICES = (
