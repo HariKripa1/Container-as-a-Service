34f8f2b7627339759a3bcdeac1156c24b408baed
kselladu@asu.edu <kselladu@asu.edu>
2016-10-26 13:42:30 -0700

    createDockerMachine script added

--- script/buildSwarm.sh
+++ script/buildSwarm.sh
@@ -13,7 +13,7 @@ router_name="router_"$user_name
 source openrc $user_name $project_name
 env|grep OS
 
-#test -f ~/.ssh/$user_name.pub || ssh-keygen -t rsa -N "" -f ~/.ssh/$user_name
+test -f ~/.ssh/$user_name.pub || ssh-keygen -t rsa -N "" -f ~/.ssh/$user_name
 #nova keypair-add --pub-key ~/.ssh/$user_name.pub $user_name"key"
 #nova secgroup-add-rule $SECURITY_GROUP_NAME icmp -1 -1 0.0.0.0/0
 #nova secgroup-add-rule $SECURITY_GROUP_NAME tcp 22 22 0.0.0.0/0
@@ -33,11 +33,15 @@ END
 x=1
 while [ $x -le $inst_no ]
 do
-    nova boot --flavor d1 --image $image_id --user-data inst-config.txt --nic net-id=$network_id --security-group default --key-name $user_name"key" $user_name"_instance_"$x
-    nova_id=$(nova list | grep $user_name"_instance_"$x | awk '{print $2}')
-    floating_ip=$(openstack floating ip create $public_net_name | awk '{print $4}')
-
-    docker-machine create -d generic --generic-ssh-user ubuntu --generic-ssh-key ~/.ssh/$user_name"key" --generic-ip-address $floating_ip dm-$user_name"_instance_"$x
+    nova boot --flavor d1 --image $image_id --user-data inst-config.txt --nic net-id=$network_id --security-group default --key-name $user_name"key" $user_name"-instance-"$x
+    nova_id=$(nova list | grep $user_name"-instance-"$x | awk '{print $2}')
+    floating_ip=$(nova floating-ip-create $public_net_name | grep $public_net_name |awk '{print $4}')
+    echo $floating_ip
+    sleep 30s
+    nova floating-ip-associate $user_name"-instance-"$x $floating_ip
+    #ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $floating_ip
+    #docker-machine create -d generic --generic-ssh-user ubuntu --generic-ssh-key ~/.ssh/$user_name".pub" --generic-ip-address $floating_ip "dm-"$user_name"-instance-"$x
+    #docker-machine regenerate-certs "dm-"$user_name"-instance-"$x
 
   x=$(( $x + 1 ))
 done
---
+++ script/createDockerMachine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DEVSTACK_PATH=~/devstack
+cd $DEVSTACK_PATH
+user_name=$1
+password=$2
+project_name=$3
+ip=$4
+instance_name=$5
+source openrc $user_name $project_name
+env|grep OS
+x=1
+while [ $x -le 200 ]
+do
+    if ssh $ip 'exit'; then
+        #echo "success"
+        break
+    else
+        sleep 10s
+    fi
+    x=$(( $x + 1 ))
+done
+if [ $x -eq 500 ]
+then
+    echo "failure"
+else
+    echo "success"
+    ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $floating_ip
+    docker-machine create -d generic --generic-ssh-user ubuntu --generic-ssh-key ~/.ssh/$user_name".pub" --generic-ip-address $floating_ip "dm-"$instance_name
+    docker-machine regenerate-certs "dm-"$user_name"-instance-"$x
+fi
