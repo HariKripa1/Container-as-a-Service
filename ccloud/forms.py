fdfd7b0041cc967eff22bb476dad021597ca3964
Your Name <ssaibab1@asu.edu>
2016-11-01 17:59:22 -0700

    added admin user

--- ccloud/forms.py
+++ ccloud/forms.py
@@ -12,15 +12,19 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-class AddPage(forms.Form):
+        
+class AddPage(forms.Form):    
     containername = forms.CharField(label='Container Name', max_length=500)
     giturl = forms.CharField(label='GIT Hub URL', max_length=500)
     port = forms.CharField(label='Port', max_length=500)    
-    
-class ModifyPage(forms.Form):    
+
+class AdminAddPage(forms.Form):
+    user = forms.ModelChoiceField(queryset = User.objects.all() )
     containername = forms.CharField(label='Container Name', max_length=500)
     giturl = forms.CharField(label='GIT Hub URL', max_length=500)
-    port = forms.CharField(label='Port', max_length=500)
+    port = forms.CharField(label='Port', max_length=500)        
+    
+class ModifyPage(forms.Form):        
     scale = forms.CharField(label='Scale', max_length=500)
 
 class AddClusterPage(forms.Form):
@@ -29,3 +33,8 @@ class AddClusterPage(forms.Form):
 
 class ModifyClusterPage(forms.Form):
     noOfNodes = forms.IntegerField(label='No of Nodes', max_value=3, min_value=1)    
+    
+class AdminAddClusterPage(forms.Form):
+    user = forms.ModelChoiceField(queryset = User.objects.all() )
+    clustername = forms.CharField(label='Cluster Name', max_length=500)
+    noOfNodes = forms.IntegerField(label='No of Nodes', max_value=3, min_value=1)
